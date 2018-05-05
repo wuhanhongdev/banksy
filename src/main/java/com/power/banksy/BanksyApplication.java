ebad20b6bf9e650ec9396721262f342c6d82bef4
wuhanhong <wuhanhong@jinhuhang.com.cn>
2018-05-05 22:52:30 +0800

    init codes

---
+++ src/main/java/com/power/banksy/BanksyApplication.java
@@ -0,0 +1,12 @@
+package com.power.banksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksyApplication.class, args);
+	}
+}
